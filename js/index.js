a2792614ea75ed20f3c8581ebb91458b79ec9215
Daniel <jorgedanielsaini@gmail.com>
2022-10-21 14:20:26 -0300

    validado form

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-/* Const */
+// Const 
 const burger = document.querySelector(".burger");
 const menu_links = document.querySelector(".menu-item");
 const bars = document.querySelectorAll(".burger span");
 const formulario = document.getElementById("formulario");
 const inputs = document.querySelectorAll("#formulario input");
+const coment = document.getElementById("coment");
 const message_error = document.querySelectorAll("#formulario p");;
 
-/* navigation bar */
+// navigation bar
 function activate_nav(){
 	menu_links.classList.toggle("activado");
 	bars.forEach(child =>{
@@ -14,21 +15,73 @@ function activate_nav(){
 	})
 }
 
-/* Form Validation */
-
-const expressions = {
-	name: /^[a-zA-zÀ-ÿ\s]{1,40}$/
-	
+// Cuenta y muestra los cáracteres del texarea "coment"
+function countCharacters() {
+	document.getElementById("counter").textContent = coment.value.length;
 }
 
-function form_validation(){
-	if(expressions || inputs[0].value.length < 4 || inputs[0].value.length >40){
-		message_error[0].classList.remove("valid-input");
-		message_error[0].classList.add("invalid-input");
-		console.log("Error")
-	} else if (inputs[0].value.length > 4 && inputs[0].value.length < 40){
-		message_error[0].classList.remove("invalid-input");
-		message_error[0].classList.add("valid-input");
-		console.log("ok")
+// Form Validation
+
+var resName = false;
+var resMail = false;
+var resWhatsApp = false;
+var resComent = false;
+
+function input_validation(expression, input, indexMessage){
+	if(expression.test(input.value)){
+		message_error[indexMessage].classList.remove("invalid-input");
+		message_error[indexMessage].classList.add("valid-input");
+		return true;
+	} else {
+		message_error[indexMessage].classList.remove("valid-input");
+		message_error[indexMessage].classList.add("invalid-input");
+		return false;
 	}
+}
+
+function form_validation(e){
+	//Expresiones regulares para realizar las validaciones
+	const expressions = {
+		name_valid: /^[a-zA-zÀ-ÿ\s]{4,40}$/,
+		mail_valid: /^[a-zA-z0-9_.+-]+@[a-zA-z0-9-]+\.[a-zA-z0-9.]+$/,
+		whatsApp_valid:/^\d{7,12}$/,
+		comment_valid: /^.{10,400}$/
+	}
+
+	switch(e.target.name){
+		case "nameForm":
+			resName = input_validation(expressions.name_valid, e.target, 0);
+		break;
+		case "mail":
+			if(e.target.value){
+				resMail = input_validation(expressions.mail_valid, e.target, 1);
+			} else {
+				message_error[1].classList.remove("invalid-input");
+				message_error[1].classList.add("valid-input");
+				resMail = true;
+			}
+		break;
+		case "whatsApp":
+			resWhatsApp = input_validation(expressions.whatsApp_valid, e.target, 2);
+		break;
+		case "coment":
+			resComent = input_validation(expressions.comment_valid, e.target, 4);
+		break;
+	}
+}
+
+inputs.forEach((input) =>{
+	input.addEventListener("blur", form_validation);
+	coment.addEventListener("blur", form_validation);
+});
+
+// Envia el formulario
+function enviar(){
+	if(resName && resMail && resWhatsApp && resComent){
+		console.log("enviado")
+	} else {
+		console.log("no enviado")
+	}
+	
+	
 }
